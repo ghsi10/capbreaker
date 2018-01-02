52b0316faf33b7df3b843b1beb1937a209b48df0
idan <ghsi10@gmail.com>
2018-01-02 21:19:54 +0200

    fix security

--- capbreakerboot/src/main/java/com/configs/SecurityConfig.java
+++ capbreakerboot/src/main/java/com/configs/SecurityConfig.java
@@ -5,42 +5,42 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
 	@Value("${spring.queries.users-query}")
 	private String usersQuery;
 	@Value("${spring.queries.roles-query}")
 	private String rolesQuery;
 
-	private DataSource dataSource;
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().antMatchers("/*").permitAll().antMatchers("/client/**")
-				.hasAnyRole("USER", "ADMIN").anyRequest().authenticated().and().httpBasic();
-	}
-
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/css/**", "/images/**", "/scripts/**", "/database/**");
-	}
-
-	@Override
-	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth, DataSource dataSource) throws Exception {
 		auth.jdbcAuthentication().dataSource(dataSource).usersByUsernameQuery(usersQuery)
 				.authoritiesByUsernameQuery(rolesQuery);
 	}
 
-	@Autowired
-	public void setDataSource(DataSource dataSource) {
-		this.dataSource = dataSource;
+	@Configuration
+	@Order(1)
+	public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.antMatcher("/client/**").authorizeRequests().anyRequest().hasAnyRole("ADMIN", "USER").and().httpBasic();
+		}
+	}
+
+	@Configuration
+	public static class FormLoginWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests().antMatchers("/admin/**").hasRole("USER").and().formLogin().and().logout();
+		}
 	}
 }
