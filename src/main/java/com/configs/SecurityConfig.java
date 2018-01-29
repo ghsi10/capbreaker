76357c051c43ab9457473b4fa8fc4fd21a32494d
istrovinsky <istrovinsky@ASIL-ISTROVINSKY.airspan.com>
2018-01-29 14:15:41 +0200

    security path fix

--- src/main/java/com/configs/SecurityConfig.java
+++ src/main/java/com/configs/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
 
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
-			http.csrf().disable().antMatcher("/client/**").authorizeRequests().anyRequest().hasAnyRole("ADMIN", "USER")
+			http.csrf().disable().antMatcher("/agent/**").authorizeRequests().anyRequest().hasAnyRole("ADMIN", "USER")
 					.and().httpBasic();
 		}
 	}
