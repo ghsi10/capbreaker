62e307ccd483c73f6f2be4bc7e2512a102b5f92b
Tamirs <tkiller91@gmail.com>
2018-02-07 13:25:34 +0200

    Get result button enabled only when both the task id and password are not empty.

--- src/main/resources/static/scripts/result.js
+++ src/main/resources/static/scripts/result.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
 	$("#inputTaskId").on('input', function() {
-		if (this.value.length > 0)
+		if (this.value.length > 0 && $("#inputTaskPassword").val().length > 0)
 			$("#getResultBtn").removeAttr("disabled");
 		else
 			$("#getResultBtn").attr("disabled", "disabled");
 	});
+
+	$("#inputTaskPassword").on('input', function() {
+        if (this.value.length > 0 && $("#inputTaskId").val().length > 0)
+            $("#getResultBtn").removeAttr("disabled");
+        else
+            $("#getResultBtn").attr("disabled", "disabled");
+    });
 });
