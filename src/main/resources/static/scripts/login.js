845986acccbd234f50ba8e44fa7bbef26c041575
istrovinsky <istrovinsky@ASIL-ISTROVINSKY.airspan.com>
2018-02-26 20:17:15 +0200

    Rewrite login style

---
+++ src/main/resources/static/scripts/login.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	$("#username").on('input', function() {
+		if (this.value.length > 0 && $("#password").val().length > 0)
+			$("#loginSbmit").removeAttr("disabled");
+		else
+			$("#loginSbmit").attr("disabled", "disabled");
+	});
+
+	$("#password").on('input', function() {
+        if (this.value.length > 0 && $("#username").val().length > 0)
+            $("#loginSbmit").removeAttr("disabled");
+        else
+            $("#loginSbmit").attr("disabled", "disabled");
+    });
+});
