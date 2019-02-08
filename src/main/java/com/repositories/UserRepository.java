cbbb3c7a8fe57fa266533cb787fed4a7b12dfe00
Idan Str <ghsi10@users.noreply.github.com>
2019-02-08 14:40:05 +0200

    Update UserRepository.java

--- src/main/java/com/repositories/UserRepository.java
+++ src/main/java/com/repositories/UserRepository.java
@@ -5,12 +5,10 @@ import com.models.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     User findOneByUsername(String username);
