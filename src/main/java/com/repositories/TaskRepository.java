f360bf982298735dde54afc29844325e40f797f5
Idan Str <ghsi10@users.noreply.github.com>
2019-02-08 14:39:51 +0200

    Update TaskRepository.java

--- src/main/java/com/repositories/TaskRepository.java
+++ src/main/java/com/repositories/TaskRepository.java
@@ -8,12 +8,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Repository
 public interface TaskRepository extends JpaRepository<Task, Integer> {
 
     Page<Task> findAllByOrderByIdDesc(Pageable pageable);
