3feb83d596474cb408e7c641f53727fd70a26577
ghsi10 <ghsiiscool@walla.com>
2019-05-25 20:40:55 +0300

    Add tests

--- src/test/java/com/exceptions/UnsupportedDataTypeExceptionTest.java
+++ src/test/java/com/exceptions/UnsupportedDataTypeExceptionTest.java
@@ -2,7 +2,10 @@ package com.exceptions;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 public class UnsupportedDataTypeExceptionTest {
 
     @Test
---
+++ src/test/java/com/models/AgentTest.java
@@ -0,0 +1,21 @@
+package com.models;
+
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class AgentTest {
+
+    private Agent agent;
+
+    @Before
+    public void init() {
+        Task task = new Task();
+        String[] commands = new String[]{"bla1", "bla2", "bla3"};
+        agent = new Agent(new Scan(task, commands));
+    }
+
+
+}
