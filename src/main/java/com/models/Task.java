a714143f59bd0896631fe192354f318115e5d3a3
idan <ghsi10@gmail.com>
2020-09-26 15:28:32 +0300

    dev branch

--- src/main/java/com/models/Task.java
+++ src/main/java/com/models/Task.java
@@ -17,6 +17,7 @@ public class Task {
     private BigDecimal progress;
     private String wifiPassword;
     private String taskPassword;
+    @Lob
     private Handshake handshake;
     private Boolean pulled;
 
